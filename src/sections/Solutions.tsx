265e48162e37f5df6417fc747cede77609e2decc
Dylan Dorusse-Répi <agence.drdigital@gmail.com>
2024-06-05 14:10:52 +0200

    correction section solution

--- src/sections/Solutions.tsx
+++ src/sections/Solutions.tsx
@@ -28,39 +28,42 @@ const Solutions = () => {
         queryFn: getHome,
     });
 
-    const {title, collectionsLink, card} = data.solutions
-
     if (isLoading) return <Loader/>
 
     if (error) return <p>{error.message}</p>
 
     return (
-        <section className="bg-white py-6 sm:py-8 lg:py-28">
-            <div className="mx-auto max-w-screen-2xl px-4 md:px-8">
-                <div className="mb-6 flex flex-col md:flex-row md:items-end md:justify-between gap-4">
-                    <h2 className="text-2xl font-bold text-gray-800 lg:text-3xl">{title}</h2>
+        <>
+            {
+                data.solutions &&
+                <section className="bg-white py-6 sm:py-8 lg:py-28">
+                    <div className="mx-auto max-w-screen-2xl px-4 md:px-8">
+                        <div className="mb-6 flex flex-col md:flex-row md:items-end md:justify-between gap-4">
+                            <h2 className="text-2xl font-bold text-gray-800 lg:text-3xl">{data.solutions.title}</h2>
 
-                    <Button
-                        label="Tout voir"
-                        url={collectionsLink}
-                        className="btn btn-white-gray"
-                    />
-                </div>
+                            <Button
+                                label="Tout voir"
+                                url={data.solutions.collectionsLink}
+                                className="btn btn-white-gray"
+                            />
+                        </div>
 
-                <div className="grid gap-x-4 gap-y-6 sm:grid-cols-2 md:gap-x-6 lg:grid-cols-3">
-                    {card.map((item:SolutionCardType) => (
-                        <SolutionCard
-                            key={item.id}
-                            url={item.url}
-                            image={item.image.data ? backUrl + item.image.data.attributes.url : emptyImg.src}
-                            alt={item.image.data ? item.image.data.attributes.alternativeText : ""}
-                            category={item.title}
-                            solution={item.label}
-                        />
-                    ))}
-                </div>
-            </div>
-        </section>
+                        <div className="grid gap-x-4 gap-y-6 sm:grid-cols-2 md:gap-x-6 lg:grid-cols-3">
+                            {data.solutions.card.map((item: SolutionCardType) => (
+                                <SolutionCard
+                                    key={item.id}
+                                    url={item.url}
+                                    image={item.image.data ? backUrl + item.image.data.attributes.url : emptyImg.src}
+                                    alt={item.image.data ? item.image.data.attributes.alternativeText : ""}
+                                    category={item.title}
+                                    solution={item.label}
+                                />
+                            ))}
+                        </div>
+                    </div>
+                </section>
+            }
+        </>
     );
 };
 

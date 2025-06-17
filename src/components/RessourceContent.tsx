f56a578d5fc3df4ab2f428944b2e1a09ab758309
Dylan Dorusse-Repi <agence.drdigital@gmail.com>
2025-06-17 13:50:35 +0200

    correction divers

--- src/components/RessourceContent.tsx
+++ src/components/RessourceContent.tsx
@@ -51,7 +51,7 @@ const RessourceContent = () => {
                                     paragraph: ({children}) =>
                                         <p className="mb-8 text-gray-600 [&>strong]:text-accent">{children}</p>,
                                     list: ({children}) =>
-                                        <ul className="list-check list-inside pb-12 text-gray-600">{children}</ul>,
+                                        <ul className="list-disc list-inside pb-12 text-gray-600">{children}</ul>,
                                     "list-item": ({children}) => (
                                         <li
                                             className={`flex items-center gap-2 pb-4 text-[1rem] check before:w-6 before:h-6 before:block before:text-red`}
@@ -74,9 +74,9 @@ const RessourceContent = () => {
                                             case 4:
                                             case 5:
                                             case 6:
-                                                return <h3 className="text-h3">{children}</h3>
+                                                return <h3 className="text-h3 pb-4">{children}</h3>
                                             default:
-                                                return <h1 className="text-h1">{children}</h1>
+                                                return <h1 className="text-h1 pb-4">{children}</h1>
                                         }
                                     },
                                     image: ({image}) => {

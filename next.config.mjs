7be817100d1878c10caf0fd169195df2c575315e
Dylan <agence.drdigital@gmail.com>
2026-06-30 20:21:00 +0200

    headers config

--- next.config.mjs
+++ next.config.mjs
@@ -1,20 +1,75 @@
 /** @type {import('next').NextConfig} */
-const backUrl = process.env.NEXT_PUBLIC_BACK_URL || process.env.BACK_URL;
-const remotePatterns = backUrl
-  ? [
-      {
-        protocol: new URL(backUrl).protocol.replace(":", ""),
-        hostname: new URL(backUrl).hostname,
-        port: new URL(backUrl).port,
-        pathname: "/**",
-      },
-    ]
-  : [];
+
+const cspHeader = `
+    upgrade-insecure-requests
+`
+
+const securityHeaders = [
+    {
+        key: "X-DNS-Prefetch-Control",
+        value: "on",
+    },
+    {
+        key: "Strict-Transport-Security",
+        value: "max-age=63072000; includeSubDomains; preload",
+    },
+    {
+        key: "X-XSS-Protection",
+        value: "1; mode=block",
+    },
+    {
+        key: "X-Frame-Options",
+        value: "sameorigin",
+    },
+    {
+        key: "Permissions-Policy",
+        value: "accelerometer=(), camera=(), cross-origin-isolated=(), geolocation=(), gyroscope=(), keyboard-map=(), magnetometer=(), microphone=(), midi=(), usb=()",
+    },
+    {
+        key: "Referrer-Policy",
+        value: "no-referrer-when-downgrade",
+    },
+    {
+        key: "X-Content-Type-Options",
+        value: "nosniff",
+    },
+];
 
 const nextConfig = {
   images: {
-    remotePatterns,
+      remotePatterns: [
+          {
+              protocol: 'https',
+              hostname: 'api.edilogic.fr',
+          },
+          {
+              protocol: 'https',
+              hostname: 'img.youtube.com',
+          },
+          {
+              protocol: 'http',
+              hostname: 'localhost',
+              port: '1337',
+          },
+      ],
   },
+    async headers() {
+        return [
+            {
+                source: '/(.*)',
+                headers: [
+                    {
+                        key: 'Content-Security-Policy',
+                        value: cspHeader.replace(/\n/g, ''),
+                    },
+                ],
+            },
+            {
+                source: '/(.*)',
+                headers: securityHeaders,
+            },
+        ]
+    },
 };
 
 export default nextConfig;
